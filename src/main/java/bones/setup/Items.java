1410fd920a8e20fe73fc45274b9e652a4aa30566
Wouter <wouter@grijsloke.be>
2019-10-10 13:03:24 +0200

    Fixed spawn egg dispense behavior

--- src/main/java/bones/setup/Items.java
+++ src/main/java/bones/setup/Items.java
@@ -12,21 +12,25 @@ import static bones.Bones.MODID;
 
 public class Items {
 
+    public static final Item SKELETON_SHEEP_SPAWN_EGG = new SpawnEggItem(Entities.SKELETON_SHEEP, 0xffffff, 15198183, new Item.Properties().group(ItemGroup.MISC))
+            .setRegistryName(new ResourceLocation(MODID, "skeleton_sheep_spawn_egg"));
+    public static final Item SKELETON_PIG_SPAWN_EGG = new SpawnEggItem(Entities.SKELETON_PIG, 0xffffff, 15771042, new Item.Properties().group(ItemGroup.MISC))
+            .setRegistryName(new ResourceLocation(MODID, "skeleton_pig_spawn_egg"));
+    public static final Item SKELETON_COW_SPAWN_EGG = new SpawnEggItem(Entities.SKELETON_COW, 0xffffff, 4470310, new Item.Properties().group(ItemGroup.MISC))
+            .setRegistryName(new ResourceLocation(MODID, "skeleton_cow_spawn_egg"));
+    public static final Item SKELETON_CHICKEN_SPAWN_EGG = new SpawnEggItem(Entities.SKELETON_CHICKEN, 0xffffff, 16711680, new Item.Properties().group(ItemGroup.MISC))
+            .setRegistryName(new ResourceLocation(MODID, "skeleton_chicken_spawn_egg"));
+
     public static final Item PORKCHOP_ON_A_STICK = new PorkchopOnAStickItem(new Item.Properties().group(ItemGroup.TRANSPORTATION).maxDamage(25))
             .setRegistryName(new ResourceLocation(Bones.MODID, "porkchop_on_a_stick"));
-
+    
     public static void register(RegistryEvent.Register<Item> event) {
-        Item.Properties eggProperties = new Item.Properties().group(ItemGroup.MISC);
         event.getRegistry().registerAll(
                 PORKCHOP_ON_A_STICK,
-                new SpawnEggItem(Entities.SKELETON_SHEEP, 0xffffff, 15198183, eggProperties)
-                        .setRegistryName(new ResourceLocation(MODID, "skeleton_sheep_spawn_egg")),
-                new SpawnEggItem(Entities.SKELETON_PIG, 0xffffff, 15771042, eggProperties)
-                        .setRegistryName(new ResourceLocation(MODID, "skeleton_pig_spawn_egg")),
-                new SpawnEggItem(Entities.SKELETON_COW, 0xffffff, 4470310, eggProperties)
-                        .setRegistryName(new ResourceLocation(MODID, "skeleton_cow_spawn_egg")),
-                new SpawnEggItem(Entities.SKELETON_CHICKEN, 0xffffff, 16711680, eggProperties)
-                        .setRegistryName(new ResourceLocation(MODID, "skeleton_chicken_spawn_egg"))
+                SKELETON_SHEEP_SPAWN_EGG,
+                SKELETON_PIG_SPAWN_EGG,
+                SKELETON_COW_SPAWN_EGG,
+                SKELETON_CHICKEN_SPAWN_EGG
         );
     }
 }
